bcaa8cb688064900a23a019c78fa5a8d43fbe2d2
김민재 <kimminje7267@naver.com>
2024-02-12 01:11:36 +0900

    feat: client측 pagination 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Providers from "@/store/provider";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center pt-12 gap-10">
+    <main className="flex min-h-screen flex-col items-center pt-12 gap-5">
       <Providers>
         <SearchWindow />
         <SongsList />
--- src/components/Songs/SongsList.tsx
+++ src/components/Songs/SongsList.tsx
@@ -1,19 +1,63 @@
-import { songsAtom } from "@/store/songs";
+"use client";
+import { useEffect, useState } from "react";
 import { useAtom } from "jotai";
+
+import { songsAtom } from "@/store/songs";
 
 const SongsList = () => {
   const [songs] = useAtom(songsAtom);
+  const [count, setCount] = useState(10);
+
+  console.log(songs);
+
+  useEffect(() => {
+    if (songs.length >= 1) {
+      setCount(count);
+    }
+  }, [count, songs.length]);
+
+  if (songs[count - 10] === undefined) return null;
 
   return (
-    <div className="flex flex-col">
-      {songs
-        .sort((a, b) => Number(b.listeners) - Number(a.listeners))
-        .map((song, i) => (
-          <a key={song.mbid ? song.mbid : Math.random() * 100 * i}>
-            {song.name}({song.artist})
-          </a>
-        ))}
-    </div>
+    <>
+      <div className="flex flex-col justify-center w-[400px] text-sm border-orange-400 border-[2px] rounded-lg p-[8px]">
+        {songs
+          .slice(count - 10, count)
+          .sort((a, b) => Number(b.listeners) - Number(a.listeners))
+          .map((song, i) => (
+            <div
+              key={song.mbid ? song.mbid : Math.random() * 100 * i}
+              className="p-2 flex flex-row justify-between"
+            >
+              <div className="overflow-hidden whitespace-nowrap overflow-ellipsis">
+                {song.name}
+              </div>
+              <div className="text-[10px] overflow-hidden whitespace-nowrap overflow-ellipsis">
+                {song.artist}
+              </div>
+            </div>
+          ))}
+      </div>
+      <div className="flex gap-2 pb-5">
+        <button
+          onClick={() => setCount((prev) => prev - 10)}
+          disabled={count === 10}
+          className="disabled:opacity-20"
+        >
+          {"<"}
+        </button>
+        <div>
+          {count / 10} / {Math.ceil(songs.length / 10)}
+        </div>
+        <button
+          onClick={() => setCount((prev) => prev + 10)}
+          disabled={count / 10 === Math.ceil(songs.length / 10)}
+          className="disabled:opacity-20"
+        >
+          {">"}
+        </button>
+      </div>
+    </>
   );
 };
 

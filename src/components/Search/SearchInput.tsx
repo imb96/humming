023579860d8b31510258b0fa49050df7d81f6e10
김민재 <kimminje7267@naver.com>
2024-02-11 14:30:05 +0900

    feat: 음악 검색 기능 추가

--- src/components/Search/SearchInput.tsx
+++ src/components/Search/SearchInput.tsx
@@ -1,11 +1,42 @@
+"use client";
+
+import { useState } from "react";
+import { songsAtom } from "@/store/songs";
+import { useSetAtom } from "jotai";
+
+import getMusic from "@/api/getMusic";
+import SearchButton from "./SearchButton";
+
 const SearchInput = () => {
+  const [input, setInput] = useState("");
+  const setSongsAtomValue = useSetAtom(songsAtom);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const res = await getMusic({
+      method: "track.search",
+      track: input,
+    });
+
+    if (res.results.trackmatches) {
+      setSongsAtomValue(res.results.trackmatches.track);
+    }
+  };
+
   return (
-    <input
-      type="text"
-      placeholder="Search your humming..."
-      className="focus:outline-none text-xs w-[100%]"
-      autoFocus
-    />
+    <>
+      <form onSubmit={handleSubmit} className="flex flex-row pl-2">
+        <input
+          type="text"
+          placeholder="Search your humming..."
+          className="focus:outline-none text-xs w-[100%]"
+          autoFocus
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <SearchButton disabled={input.length < 2} />
+      </form>
+    </>
   );
 };
 
--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,10 +1,8 @@
-import SearchIcon from "./SearchIcon";
 import SearchInput from "./SearchInput";
 
 const Search = () => {
   return (
-    <div className="flex items-center gap-2 border-solid focus-within:border-orange-400 border-[2px] rounded-full p-[8px] min-w-[240px]">
-      <SearchIcon />
+    <div className="border-solid focus-within:border-orange-400 border-[2px] rounded-full p-[8px] min-w-[240px]">
       <SearchInput />
     </div>
   );

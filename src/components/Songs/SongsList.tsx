1f4d603522b00403fab4dcc0dfd8e34bf5fa7a6f
김민재 <kimminje7267@naver.com>
2024-02-12 16:04:07 +0900

    refactor: SongsList 내부 컴포넌트 분리

--- src/components/Songs/SongsList.tsx
+++ src/components/Songs/SongsList.tsx
@@ -3,7 +3,9 @@ import { useEffect, useState } from "react";
 import { useAtom } from "jotai";
 
 import { songsAtom } from "@/store/songs";
-import Image from "next/image";
+
+import SongCard from "./SongCard";
+import SongsPagination from "./SongsPagination";
 
 const SongsList = () => {
   const [songs] = useAtom(songsAtom);
@@ -24,50 +26,17 @@ const SongsList = () => {
           .slice(count - 10, count)
           .sort((a, b) => Number(b.listeners) - Number(a.listeners))
           .map((song, i) => (
-            <div
+            <SongCard
+              song={song}
               key={song.mbid ? song.mbid : Math.random() * 100 * i}
-              className="p-2 flex flex-row justify-between"
-            >
-              <div className="flex flex-row gap-2 items-center">
-                <Image
-                  src={
-                    song.image[0]["#text"]
-                      ? song.image[0]["#text"]
-                      : "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png"
-                  }
-                  alt="album image"
-                  width={32}
-                  height={32}
-                />
-                <div className="overflow-hidden whitespace-nowrap overflow-ellipsis">
-                  {song.name}
-                </div>
-              </div>
-              <div className="text-[10px] overflow-hidden whitespace-nowrap overflow-ellipsis">
-                {song.artist}
-              </div>
-            </div>
+            />
           ))}
       </div>
-      <div className="flex gap-2 pb-5">
-        <button
-          onClick={() => setCount((prev) => prev - 10)}
-          disabled={count === 10}
-          className="disabled:opacity-20"
-        >
-          {"<"}
-        </button>
-        <div>
-          {count / 10} / {Math.ceil(songs.length / 10)}
-        </div>
-        <button
-          onClick={() => setCount((prev) => prev + 10)}
-          disabled={count / 10 === Math.ceil(songs.length / 10)}
-          className="disabled:opacity-20"
-        >
-          {">"}
-        </button>
-      </div>
+      <SongsPagination
+        count={count}
+        setCount={setCount}
+        length={songs.length}
+      />
     </>
   );
 };

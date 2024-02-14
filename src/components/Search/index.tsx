8aab1de2378e360a62b878818ec121c7177b18f3
김민재 <kimminje7267@naver.com>
2024-02-14 12:45:25 +0900

    feat: 검색 유형에 따른 검색 로직 구현

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,7 +2,7 @@ import SearchInput from './SearchInput'
 
 const Search = () => {
   return (
-    <div className="border-solid focus-within:border-orange-400 border-[2px] rounded-full p-[8px] min-w-[240px]">
+    <div className="flex items-center border-solid focus-within:border-orange-400 border-[2px] rounded-full p-[8px] min-w-[360px] h-[50px]">
       <SearchInput />
     </div>
   )

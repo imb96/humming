12ed8b7b316b402be655bb024fcde70013bfbb8a
김민재 <kimminje7267@naver.com>
2024-02-15 18:42:39 +0900

    feat: artist type 설정

---
+++ src/types/artist.ts
@@ -0,0 +1,30 @@
+export interface Root {
+  artists: Artists
+}
+
+export interface Artists {
+  artist: Artist[]
+  '@attr': Attr
+}
+
+export interface Artist {
+  name: string
+  playcount: string
+  listeners: string
+  mbid: string
+  url: string
+  streamable: string
+  image: Image[]
+}
+
+export interface Image {
+  '#text': string
+  size: string
+}
+
+export interface Attr {
+  page: string
+  perPage: string
+  totalPages: string
+  total: string
+}

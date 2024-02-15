a2eff2c45e5bd4da5f8e902e4bbdca7433bb69e2
김민재 <kimminje7267@naver.com>
2024-02-15 18:43:09 +0900

    feat: TopArtistList 컴포넌트 작성

---
+++ src/components/Artist/TopArtistList.tsx
@@ -0,0 +1,40 @@
+import getTopArtist from '@/api/getTopArtist'
+import { Artist } from '@/types/artist'
+
+import ArtistCard from './ArtistCard'
+
+const TopArtistList = async () => {
+  const res = await getTopArtist()
+  const topArtists = res.artists.artist.map((artist: Artist) => {
+    return {
+      image: artist.image,
+      listeners: artist.listeners,
+      mbid: artist.mbid,
+      name: artist.name,
+      streamable: '',
+      url: artist.url,
+    }
+  })
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="w-[500px] text-sm border-orange-400 border-[2px] rounded-lg p-[0 8px 8px 8px] h-[510px] overflow-auto scroll-smooth scrollbar-hide">
+        <div className="flex sticky top-0 py-[8px] pl-[8px] font-semibold bg-orange-100 items-center gap-2">
+          {'Top Artist 50 🚀'}
+          <span className="text-xs font-light">Click to watch the video!</span>
+        </div>
+        {topArtists
+          .filter((item: Artist) => item.name !== '(null)')
+          .map((artist: Artist, i: number) => (
+            <ArtistCard
+              artist={artist}
+              key={artist.mbid ? artist.mbid : Math.random() * 100 * i}
+              rank={i + 1}
+            />
+          ))}
+      </div>
+    </div>
+  )
+}
+
+export default TopArtistList

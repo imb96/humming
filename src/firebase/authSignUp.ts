d4f8f2aab6027f4fecf2b422d40093ff4c7824c2
김민재 <kimminje7267@naver.com>
2024-02-18 14:44:57 +0900

    feat: 회원가입 로직 추가
    - 회원가입 성공시 atom에 저장, 홈으로 라우팅

--- src/firebase/authSignUp.ts
+++ src/firebase/authSignUp.ts
@@ -3,19 +3,15 @@ import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
 import { app } from '@/firebase'
 import { UserAuth } from '@/types/user'
 
-const authSignUp = ({ email, password }: UserAuth) => {
+const authSignUp = async ({ email, password }: UserAuth) => {
   const auth = getAuth(app)
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user
-      console.log(user)
-    })
-    .catch((error) => {
-      const errorCode = error.code
-      const errorMessage = error.message
-      throw new Error(`[${errorCode}] ${errorMessage}`)
-    })
+  try {
+    const user = await createUserWithEmailAndPassword(auth, email, password)
+    return user
+  } catch (error) {
+    throw new Error('[authSignUp] Error')
+  }
 }
 
 export default authSignUp

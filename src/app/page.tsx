d03c8ddcec498cb797fe71a23ad7b28f17260d43
김민재 <kimminje7267@naver.com>
2024-08-17 16:39:56 +0900

    refactor: delete cards from the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,6 @@ export default function Home() {
     <div className="flex justify-center gap-5">
       <div className="flex flex-col items-center">
         <Header />
-        <div className="flex flex-col items-center gap-5 md:flex-row">
-          <TopArtistsCard />
-          <TopSongsCard />
-        </div>
       </div>
     </div>
   )

457e68c7e9e6fd107e3b0d766ccc429da2940a60
김민재 <kimminje7267@naver.com>
2024-08-17 16:38:46 +0900

    chore: add baseUrl

--- api/baseUrl.ts
+++ api/baseUrl.ts
@@ -1,2 +1,3 @@
 export const BASE_URL_LAST = `https://ws.audioscrobbler.com/2.0/?api_key=${process.env.NEXT_PUBLIC_API_KEY}&format=json`
 export const BASE_URL_YT = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&type=video&key=${process.env.NEXT_PUBLIC_YT_API_KEY}`
+export const SHAZAM_BASE_URL = `https://shazam.p.rapidapi.com`

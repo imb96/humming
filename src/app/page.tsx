1df4f63b5051a78cd38681bf60dbac0744c042f3
김민재 <kimminje7267@naver.com>
2024-10-03 18:05:39 +0900

    refactor: 불필요한 코드, 파일 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import SearchInput from '@/components/Search/SearchInput'
 
 export default function Home() {
-  return (
-    <div className="flex justify-center gap-5">
-      <div className="flex flex-col items-center">
-        <SearchInput />
-      </div>
-    </div>
-  )
+  return <SearchInput />
 }
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -5,7 +5,7 @@ import AuthContent from './Auth'
 
 export function Menu() {
   return (
-    <Menubar className="flex items-center justify-between rounded-none border-b border-none px-2 lg:px-4">
+    <Menubar className="flex items-center justify-between rounded-none border-b border-none px-2 py-8 lg:px-4">
       <Logo />
       <AuthContent />
     </Menubar>
